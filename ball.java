c4e4a2a475b897bf267320bc0fd85195557abffb
mhchandesu <163235065+mhchandesu@users.noreply.github.com>
2024-04-17 15:24:05 +0900

    Create ball.java

---
+++ ball.java
@@ -0,0 +1,15 @@
+public class ball extends Circle {
+		private String color;
+		
+		public Ball(String color) {
+			this.color = color;
+		}
+		
+		public void findColor() {
+			System.out.println(color + "공이다.");
+		}
+		
+		public void findVolume() {
+			System.out.println("부피는 4/3*(파이*반지름*반지름*반지름)이다.");
+		}
+}
