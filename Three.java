23dd3daa0f5fe830192b06125508be0f5855759b
mhchandesu <163235065+mhchandesu@users.noreply.github.com>
2024-04-17 15:47:58 +0900

    Create Three.java

---
+++ Three.java
@@ -0,0 +1,12 @@
+package sec06.other;
+
+import sec06.One;
+
+public class Three {
+	void print() {
+		One o = new One();
+		//System.out.println(o.secret);
+		//System.out.println(o.roomate);
+		System.out.println(o.anybody);
+	}
+}
